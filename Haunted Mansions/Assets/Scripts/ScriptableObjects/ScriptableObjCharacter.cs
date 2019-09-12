66e531d806d02cd05335fc0ae7849a2cb5f3d4fe
MindlessElle <elliot.angman@gmail.com>
2019-09-12 20:49:32 +0200

    Renamed default scene to main Added character ScriptableObject Added more folders

---
+++ Haunted Mansions/Assets/Scripts/ScriptableObjects/ScriptableObjCharacter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "RPG Assets/New Character", order = 0)]
+public class ScriptableObjCharacter : ScriptableObject
+{
+    public Texture2D PreviewImage;
+    public new string name;
+    public float health;
+    public float armour;
+    public float speed;
+    public int intelligence;
+    public int dexterity;
+}
